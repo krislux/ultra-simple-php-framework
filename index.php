ef3b4df0927ab50976334e3f0de53364275c804c
Kris Lux <kl@republica.dk>
2018-09-17 12:17:28 +0200

    No longer blocks other autoloaders

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 
 define('ROOT_DIR', __DIR__);
-
-spl_autoload_register(function($name) {
-    require "inc/$name.php";
-});
-
-function dd() {
-    var_dump(func_get_args());
-    exit;
-};
-
-function request($key, $default = '') {
-    return isset($_REQUEST[$key]) ? $_REQUEST[$key] : $default;
-}
+define('ROOT_URL', dirname($_SERVER['SCRIPT_NAME']));
 
 (function() {
+    spl_autoload_register(function($name) {
+        $fn = "inc/$name.php";
+        if (file_exists($fn))
+            require $fn;
+    });
+    
+    function dd() {
+        var_dump(func_get_args());
+        exit;
+    };
+    
+    function request($key, $default = '') {
+        return isset($_REQUEST[$key]) ? $_REQUEST[$key] : $default;
+    }
+
     require 'Controller.php';
     $controller = new Controller;
     $rv = $controller->dispatch($_SERVER['REQUEST_METHOD'], request('page'));
